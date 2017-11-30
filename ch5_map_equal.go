6e94bb824765dda206b448ba78390c8544b684ac
高新建 <gaoxj@gaoxinjiandeMac-mini.local>
2017-11-30 12:59:35 +0800

    add ch5_map_equal

---
+++ ch5_map_equal.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func equal(x, y map[string]int) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	for k, xv := range x {
+		if yv, ok := y[k]; !ok || yv != xv {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	m1 := map[string]int{
+		"a": 1,
+		"b": 2,
+		"c": 3,
+	}
+
+	m2 := map[string]int{
+		"a": 1,
+		"b": 20,
+		"c": 3,
+	}
+
+	fmt.Print(equal(m1, m2))
+}
